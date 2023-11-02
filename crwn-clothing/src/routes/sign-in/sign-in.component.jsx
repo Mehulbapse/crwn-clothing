e3806bab06f3cb4bc1ff27584c236a28ccd0e4e6
Mehulbapse <bapsemehul123@gmail.com>
2023-11-02 15:32:55 +0530

    sign up form added

--- crwn-clothing/src/routes/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,20 +1,34 @@
-import { signInWithGooglePopup } from "../../utils/firebase/firebase.utils"
 
-const SignIn = () =>{
-    const logGoogleUser = async ()=>{
-        const  response = await signInWithGooglePopup();
-        console.log(response)
-    }
-    return (
-        <div>
-            <h1>
-                In Sign In 
-            </h1>
-            <button onClick={logGoogleUser}>
-                Sign in With Google
-            </button>
-        </div>
-    )
-}
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import {
+  signInWithGooglePopup,
+  createUserDocumentFromAuth,
+} from "../../utils/firebase/firebase.utils";
 
-export default SignIn
+const SignIn = () => {
+  
+
+  const logGoogleUser = async () => {
+    const { user } = await signInWithGooglePopup();
+    const userDocRef = await createUserDocumentFromAuth(user);
+    console.log(user);
+  };
+
+  //   const logGoogleRedirectUser = async () => {
+  //     const { user } = await signInWithGoogleRedirect();
+  //     console.log({ user });
+  //   };
+  return (
+    <div>
+      <h1>In Sign In</h1>
+      <button onClick={logGoogleUser}>Sign in With Google</button>
+
+      <div>
+        <SignUpForm />
+      </div>
+    </div>
+
+  );
+};
+
+export default SignIn;
