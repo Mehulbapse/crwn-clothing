e6353f98eea142d00d10371e905cf1ca046d4a6a
Mehulbapse <bapsemehul123@gmail.com>
2023-11-09 15:13:37 +0530

    product provider added

--- crwn-clothing/src/index.js
+++ crwn-clothing/src/index.js
@@ -4,13 +4,17 @@ import "./index.scss";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { UserProvider } from "./contexts/user.context";
+import { ProductsProvider } from "./contexts/products.context";
+
 const rootElement = document.getElementById("root");
 
 render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductsProvider>
+          <App />
+        </ProductsProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>,
